82bb47ef642a78e2c128e53ab499af4d5bd4874b
raul <rauld.garcia95@gmail.com>
2025-11-10 12:38:46 -0600

    Hola mundo

---
+++ src/Titulo.tsx
@@ -0,0 +1,10 @@
+function Titulo() {
+  // jsx -> React.createElement
+  const nombre = "Pancho";
+  if (nombre) {
+    return <h1>Hola {nombre}</h1>;
+  }
+  return <h1>Hola Mundo</h1>;
+}
+
+export default Titulo;
