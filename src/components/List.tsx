30a39837e744b50a895486b2488e8d841b60dac4
raul <rauld.garcia95@gmail.com>
2025-11-12 12:13:45 -0600

    Gestion de estado

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,27 +1,28 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 type Props = {
     data: string[];
 }
 
 function List({ data }: Props) {
-    const handleClick = (event: string) => {
-        console.log(event);
+    const [index, setIndex] = useState(1);
+    const handleClick = (i: number) => {
+        setIndex(i);
     }
-  return (
-    <ul className="list-group">
-        {data.map((elemento) => (
-        <li 
-            onClick={() => handleClick(elemento)} 
-            key={elemento} 
-            className="list-group-item"
-        >
-            {elemento}
-        </li>
-        ))}
-        
-    </ul>
-  )
+    return (
+        <ul className="list-group">
+            {data.map((elemento, i) => (
+            <li 
+                onClick={() => handleClick(i)} 
+                key={elemento} 
+                className={`list-group-item ${index == i ? 'active' : ""}`}
+            >
+                {elemento}
+            </li>
+            ))}
+            
+        </ul>
+    )
 }
 
 export default List
