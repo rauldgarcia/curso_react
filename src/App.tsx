52a2f6945de1116869221fe25cf45602377750cc
raul <rauld.garcia95@gmail.com>
2025-11-12 11:43:00 -0600

    Se agregan listas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 
 function App() {
+  const list = ["Pancho", "Nambre", "Meryjein"]
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto"/>
+      <List data={list} />
     </Card>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,16 @@
+type Props = {
+    data: string[];
+}
+
+function List({ data }: Props) {
+  return (
+    <ul className="list-group">
+        {data.map((elemento) => (
+        <li key={elemento} className="list-group-item">{elemento}</li>
+        ))}
+        
+    </ul>
+  )
+}
+
+export default List
