f5b579bf6f72bf494994cf7c57a47834722b32a7
raul <rauld.garcia95@gmail.com>
2025-11-12 11:56:56 -0600

    Gestion de eventos

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,23 @@
+import { MouseEvent } from "react";
+
 type Props = {
     data: string[];
 }
 
 function List({ data }: Props) {
+    const handleClick = (event: string) => {
+        console.log(event);
+    }
   return (
     <ul className="list-group">
         {data.map((elemento) => (
-        <li key={elemento} className="list-group-item">{elemento}</li>
+        <li 
+            onClick={() => handleClick(elemento)} 
+            key={elemento} 
+            className="list-group-item"
+        >
+            {elemento}
+        </li>
         ))}
         
     </ul>
