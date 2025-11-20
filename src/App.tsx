95fa74349b56934496c403f50485d3dbbb592189
raul <rauld.garcia95@gmail.com>
2025-11-20 12:58:19 -0600

    Renderizado condicional

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
-  const list = ["Pancho", "Nambre", "Meryjein"]
+  const list: string[] = ["Pancho", "Nambre", "Meryjein"];
 
   const handleSelect = (elemento: string) => {
     console.log("Imprimiendo ", elemento)
-  }
+  };
+
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto"/>
-      <List data={list} onSelect={handleSelect} />
+      {list.length !== 0 && <List data={list} onSelect={handleSelect} />}
     </Card>
   );
 }
