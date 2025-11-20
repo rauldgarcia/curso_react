6d89efcdda55567d8414c86532fb95a5e6aeaddd
raul <rauld.garcia95@gmail.com>
2025-11-20 17:00:19 -0600

    Boton de carga

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
+import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => setIsLoading(!isLoading);
+
   const list: string[] = ["Pancho", "Nambre", "Meryjein"];
 
   const handleSelect = (elemento: string) => {
@@ -12,6 +17,12 @@ function App() {
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto"/>
       {list.length !== 0 && <List data={list} onSelect={handleSelect} />}
+      <Button 
+        isLoading = {isLoading} 
+        onClick = {handleClick}
+      >
+        Boton
+      </Button>
     </Card>
   );
 }
